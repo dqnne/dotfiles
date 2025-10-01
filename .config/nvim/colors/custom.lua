f2c413e542658ccdcab51983fb39251f8008f933
dqnne <173300634+dqnne@users.noreply.github.com>
2025-10-01 23:07:32 +0200

    nvim: revamp colorscheme

--- .config/nvim/colors/custom.lua
+++ .config/nvim/colors/custom.lua
@@ -1,23 +1,17 @@
 local p = {
-  color_0 = '#cee5e5',
-  color_1 = '#b7cccc',
-  color_2 = '#a0b2b2',
-  color_3 = '#889999',
-  color_4 = '#727f7f',
-  color_5 = '#5b6666',
+  fg = '#edf0f2',
+  fg2 = '#dbd9d0',
+  fg3 = '#c5c9bd',
+  fg4 = '#aebab0',
+  fg5 = '#99a2a3',
+  fg6 = '#86898e',
 
-  gray_0 = '#ffffff',
-  gray_1 = '#e8e8e8',
-  gray_2 = '#c8c8c8',
-  gray_3 = '#a8a8a8',
-  gray_4 = '#888888',
-  gray_5 = '#787878',
-  gray_6 = '#585858',
-  gray_7 = '#484848',
-  gray_8 = '#383838',
-  gray_9 = '#282828',
-  gray_10 = '#181818',
-  gray_11 = '#101010',
+  bg = '#0a0f14',
+  bg2 = '#161a21',
+  bg3 = '#272c33',
+  bg4 = '#3f424c',
+  bg5 = '#545860',
+  bg6 = '#686d72',
 
   light_red = '#b77979',
   light_green = '#79b779',
@@ -42,85 +36,84 @@ local function enable_colorscheme()
   vim.g.colors_name = 'custom'
 
   hi('Added',                    { fg = p.light_green })
-  hi('Boolean',                  { fg = p.gray_2 })
+  hi('Boolean',                  { fg = p.fg3 })
   hi('Changed',                  { fg = p.light_cyan })
-  hi('Character',                { fg = p.color_2, italic = true })
-  hi('Comment',                  { fg = p.color_5, italic = true })
-  hi('Constant',                 { fg = p.color_2 })
-  hi('Delimiter',                { fg = p.color_4 })
+  hi('Character',                { fg = p.fg4, italic = true })
+  hi('Comment',                  { fg = p.bg5, italic = true })
+  hi('Constant',                 { fg = p.fg4 })
+  hi('Delimiter',                { fg = p.bg6 })
   hi('Error',                    { fg = p.light_red })
-  hi('Float',                    { fg = p.color_2 })
-  hi('Function',                 { fg = p.color_3 })
-  hi('Identifier',               { fg = p.gray_5 })
-  hi('Keyword',                  { fg = p.color_1 })
-  hi('Number',                   { fg = p.color_2 })
-  hi('Operator',                 { fg = p.color_4 })
-  hi('PreProc',                  { fg = p.gray_3 })
+  hi('Float',                    { fg = p.fg4 })
+  hi('Function',                 { fg = p.fg5 })
+  hi('Identifier',               { fg = p.fg6 })
+  hi('Keyword',                  { fg = p.fg3 })
+  hi('Number',                   { fg = p.fg4 })
+  hi('Operator',                 { fg = p.bg6 })
+  hi('PreProc',                  { fg = p.fg4 })
   hi('Removed',                  { fg = p.light_red })
-  hi('Special',                  { fg = p.gray_2 })
-  hi('SpecialComment',           { fg = p.color_5 })
-  hi('Statement',                { fg = p.color_0, bold = true })
-  hi('String',                   { fg = p.color_2 })
-  hi('Type',                     { fg = p.gray_4 })
+  hi('Special',                  { fg = p.fg3 })
+  hi('Statement',                { fg = p.fg3 })
+  hi('String',                   { fg = p.fg4 })
+  hi('Type',                     { fg = p.fg5 })
 
-  hi('ColorColumn',              { bg = p.gray_9 })
-  hi('Conceal',                  { fg = p.color_5, bold = true, italic = true })
-  hi('CurSearch',                { fg = p.gray_11, bg = p.gray_0, bold = true })
-  hi('Cursor',                   { fg = p.gray_11, bg = p.color_0 })
-  hi('CursorColumn',             { bg = p.gray_9 })
-  hi('CursorLine',               { bg = p.gray_9 })
-  hi('CursorLineNr',             { fg = p.color_0, bg = p.gray_9, bold = true })
-  hi('CursorLineSign',           { bg = p.gray_9 })
+  hi('ColorColumn',              { bg = p.bg3 })
+  hi('Conceal',                  { fg = p.fg6, bold = true, italic = true })
+  hi('CurSearch',                { fg = p.bg, bg = p.fg, bold = true })
+  hi('Cursor',                   { fg = p.bg, bg = p.fg2 })
+  hi('CursorColumn',             { bg = p.bg3 })
+  hi('CursorLine',               { bg = p.bg3 })
+  hi('CursorLineNr',             { fg = p.fg2, bg = p.bg3, bold = true })
+  hi('CursorLineSign',           { bg = p.bg3 })
   hi('DiffAdd',                  { bg = p.dark_green })
   hi('DiffChange',               { bg = p.dark_cyan })
   hi('DiffDelete',               { bg = p.dark_red })
   hi('DiffText',                 { bg = p.dark_yellow })
-  hi('Directory',                { fg = p.color_3, bold = true })
-  hi('EndOfBuffer',              { fg = p.gray_6 })
+  hi('Directory',                { fg = p.fg4 })
+  hi('EndOfBuffer',              { fg = p.bg6 })
   hi('ErrorMsg',                 { fg = p.light_red })
-  hi('FloatBorder',              { fg = p.color_5 })
-  hi('FloatTitle',               { fg = p.gray_3, bg = p.gray_8, bold = true })
-  hi('FoldColumn',               { fg = p.color_5 })
-  hi('Folded',                   { fg = p.gray_2 })
-  hi('IncSearch',                { fg = p.gray_11, bg = p.gray_0, bold = true })
-  hi('LineNr',                   { fg = p.color_5 })
-  hi('LspInlayHint',             { fg = p.gray_5, bg = p.gray_9 })
-  hi('LspReferenceText',         { bg = p.gray_9 })
-  hi('MatchParen',               { fg = p.gray_0, bg = p.gray_6 })
-  hi('ModeMsg',                  { fg = p.gray_0, bg = p.gray_11, bold = true })
+  hi('FloatBorder',              { fg = p.bg5 })
+  hi('FloatTitle',               { fg = p.fg4, bg = p.bg4, bold = true })
+  hi('FoldColumn',               { fg = p.fg6 })
+  hi('Folded',                   { fg = p.fg3 })
+  hi('IncSearch',                { fg = p.bg, bg = p.fg, bold = true })
+  hi('LineNr',                   { fg = p.bg5 })
+  hi('LspInlayHint',             { fg = p.fg6, bg = p.bg3 })
+  hi('LspReferenceText',         { bg = p.bg3 })
+  hi('MatchParen',               { fg = p.fg, bg = p.bg5 })
+  hi('ModeMsg',                  { fg = p.fg2, bold = true })
   hi('MoreMsg',                  { fg = p.light_green, bold = true })
-  hi('NonText',                  { fg = p.gray_9 })
-  hi('Normal',                   { fg = p.gray_1, bg = p.gray_11 })
-  hi('NormalFloat',              { bg = p.gray_10 })
-  hi('Pmenu',                    { bg = p.gray_8 })
-  hi('PmenuSbar',                { bg = p.gray_6 })
-  hi('PmenuSel',                 { bg = p.gray_7 })
-  hi('PmenuThumb',               { bg = p.gray_5 })
+  hi('NonText',                  { fg = p.bg4 })
+  hi('Normal',                   { fg = p.fg2, bg = p.bg })
+  hi('NormalFloat',              { bg = p.bg2 })
+  hi('Pmenu',                    { bg = p.bg4 })
+  hi('PmenuSbar',                { bg = p.bg6 })
+  hi('PmenuSel',                 { bg = p.bg5 })
+  hi('PmenuThumb',               { bg = p.fg6 })
   hi('Question',                 { fg = p.light_green, bold = true })
-  hi('QuickFixLine',             { fg = p.color_0 })
-  hi('Search',                   { fg = p.gray_0, bg = p.gray_6 })
-  hi('SignColumn',               { fg = p.color_5 })
-  hi('SpecialKey',               { fg = p.gray_6, italic = true })
+  hi('QuickFixLine',             { fg = p.fg2 })
+  hi('Search',                   { fg = p.fg, bg = p.bg6 })
+  hi('SignColumn',               { fg = p.fg6 })
+  hi('SpecialKey',               { fg = p.bg6, italic = true })
   hi('SpellBad',                 { fg = p.light_red, undercurl = true })
   hi('SpellCap',                 { fg = p.light_red, undercurl = true })
   hi('SpellLocal',               { fg = p.light_red, undercurl = true })
   hi('SpellRare',                { fg = p.light_red, undercurl = true })
-  hi('StatusLine',               { fg = p.gray_0, bg = p.gray_9 })
-  hi('StatusLineNC',             { fg = p.gray_3, bg = p.gray_9 })
-  hi('Substitute',               { fg = p.gray_0, bg = p.color_5 })
-  hi('TabLine',                  { fg = p.gray_2, bg = p.gray_8 })
-  hi('TabLineFill',              { fg = p.gray_1, bg = p.gray_9 })
+  hi('StatusLine',               { fg = p.fg5, bg = p.bg3 })
+  hi('StatusLineNC',             { fg = p.bg6, bg = p.bg3 })
+  hi('Substitute',               { fg = p.fg, bg = p.fg6 })
+  hi('TabLine',                  { fg = p.fg3, bg = p.bg4 })
+  hi('TabLineFill',              { fg = p.fg2, bg = p.bg3 })
   hi('TabLineSel',               { bold = true })
-  hi('TermCursor',               { fg = p.gray_9, bg = p.gray_0 })
-  hi('Title',                    { fg = p.gray_3, bold = true })
-  hi('Visual',                   { fg = p.gray_11, bg = p.color_2 })
+  hi('TermCursor',               { fg = p.bg3, bg = p.fg })
+  hi('Title',                    { fg = p.fg4, bold = true })
+  hi('Visual',                   { bg = p.bg4 })
   hi('WarningMsg',               { fg = p.light_yellow })
-  hi('Whitespace',               { fg = p.gray_9 })
-  hi('WildMenu',                 { fg = p.gray_11, bg = p.gray_6 })
-  hi('WinBar',                   { fg = p.gray_3, bg = p.gray_8 })
-  hi('WinBarNC',                 { fg = p.gray_1, bg = p.gray_9 })
-  hi('WinSeparator',             { fg = p.gray_7, bg = p.gray_9 })
-  hi('lCursor',                  { fg = p.gray_9, bg = p.gray_2 })
+  hi('Whitespace',               { fg = p.bg3 })
+  hi('WildMenu',                 { fg = p.bg, bg = p.bg6 })
+  hi('WinBar',                   { fg = p.fg4, bg = p.bg4 })
+  hi('WinBarNC',                 { fg = p.fg2, bg = p.bg3 })
+  hi('WinSeparator',             { fg = p.bg5, bg = p.bg3 })
+  hi('lCursor',                  { fg = p.bg3, bg = p.fg3 })
 
   hi('DiagnosticError',          { fg = p.light_red })
   hi('DiagnosticHint',           { fg = p.light_magenta })
@@ -200,23 +193,23 @@ local function enable_colorscheme()
   hi('@lsp.type.typeParameter',  { link='TypeDef' })
   hi('@lsp.type.variable',       { link='@variable' })
 
-  vim.g.terminal_color_0 = p.gray_9
+  vim.g.terminal_color_0 = p.bg3
   vim.g.terminal_color_1 = p.light_red
   vim.g.terminal_color_2 = p.light_green
   vim.g.terminal_color_3 = p.light_yellow
   vim.g.terminal_color_4 = p.light_blue
   vim.g.terminal_color_5 = p.light_magenta
   vim.g.terminal_color_6 = p.light_cyan
-  vim.g.terminal_color_7 = p.gray_1
+  vim.g.terminal_color_7 = p.fg2
 
-  vim.g.terminal_color_8 = p.gray_8
+  vim.g.terminal_color_8 = p.bg4
   vim.g.terminal_color_9 = p.light_red
   vim.g.terminal_color_10 = p.light_green
   vim.g.terminal_color_11 = p.light_yellow
   vim.g.terminal_color_12 = p.light_blue
   vim.g.terminal_color_13 = p.light_magenta
   vim.g.terminal_color_14 = p.light_cyan
-  vim.g.terminal_color_15 = p.gray_0
+  vim.g.terminal_color_15 = p.fg
 end
 
 enable_colorscheme()
