5af366a86a94b2ee1085457a75380d0403f3d7a6
dqnne <173300634+dqnne@users.noreply.github.com>
2025-12-25 21:50:05 +0100

    nvim: use default nvim-treesitter branch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,7 +4,7 @@ vim.pack.add({
   'https://github.com/rafamadriz/friendly-snippets',
   'https://github.com/nvim-mini/mini.nvim',
   'https://github.com/neovim/nvim-lspconfig',
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
+  'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/stevearc/oil.nvim',
   'https://github.com/tpope/vim-dispatch',
   'https://github.com/tpope/vim-fugitive',
