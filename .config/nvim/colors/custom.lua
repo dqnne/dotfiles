88362edace1763e93185f76cbcd242159e92b9fc
dqnne <173300634+dqnne@users.noreply.github.com>
2026-04-22 21:41:38 +0200

    nvim, ghostty: adjust colors

--- .config/nvim/colors/custom.lua
+++ .config/nvim/colors/custom.lua
@@ -1,10 +1,11 @@
 local palette = require('mini.hues').make_palette({
-  background = '#0e1214',
-  foreground = '#d8d4cd',
+  background = '#171319',
+  foreground = '#d7d5cd',
+  saturation = 'lowmedium',
+  accent = 'fg',
 })
 
-palette.accent = '#d8d4cd'
-palette.accent_bg = '#1c2428'
+palette.accent_bg = '#2f2733'
 
 require('mini.hues').apply_palette(palette)
 vim.g.colors_name = 'custom'
