c578135785689792ef172f2923dd7ce7cfcf0f2d
dqnne <173300634+dqnne@users.noreply.github.com>
2026-06-07 23:16:10 +0200

    nvim: add unnest.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.pack.add({
   'https://github.com/nvim-mini/mini.nvim',
   'https://github.com/neovim/nvim-lspconfig',
   'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/brianhuster/unnest.nvim',
   'https://github.com/tpope/vim-dispatch',
   'https://github.com/tpope/vim-sleuth',
   'https://github.com/zenbones-theme/zenbones.nvim',
