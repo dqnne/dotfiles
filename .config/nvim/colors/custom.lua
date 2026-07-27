606dcf430b832c717587ca570ebf22fdc16dede6
dqnne <173300634+dqnne@users.noreply.github.com>
2026-07-27 15:39:30 +0200

    nvim: keep syntax highlighting in diffs

--- .config/nvim/colors/custom.lua
+++ .config/nvim/colors/custom.lua
@@ -2,3 +2,6 @@ vim.cmd('highlight clear')
 local hl = vim.api.nvim_set_hl
 hl(0, 'Identifier', { fg = 'NvimLightMagenta' })
 hl(0, 'Function', { fg = 'NvimLightBlue' })
+hl(0, 'DiffAdd', { bg = 'NvimDarkGreen', fg = 'none' })
+hl(0, 'DiffChange', { bg = 'NvimDarkGray4', fg = 'none' })
+hl(0, 'DiffText', { bg = 'NvimDarkCyan', fg = 'none' })
